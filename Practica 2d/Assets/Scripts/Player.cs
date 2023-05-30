4dadcec60f2449939f0fc8235928adc2074c0039
Angel Juarez Garcia <Angel_IS2020@hotmail.com>
2023-05-29 22:36:46 -0600

    se implementa salto

---
+++ Practica 2d/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator animator;
+    public float jumpForce;
+    private Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("IsJumping", true);
+            rigidbody2d.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.tag == "Floor")
+        {
+            animator.SetBool("IsJumping", false);
+        }
+    }
+}
