1c56e04982f18e1b10da54ce61b8c7977c56beb0
Angel Juarez Garcia <Angel_IS2020@hotmail.com>
2023-06-01 20:35:23 -0600

    Avance

--- Platformer2d/Assets/scripts/PlayerMovement.cs
+++ Platformer2d/Assets/scripts/PlayerMovement.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float Salto = 0;
+    private Rigidbody2D personaje;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        personaje = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        //float directionX = Input.GetAxis("Horizontal");
+        float directionX = Input.GetAxisRaw("Horizontal"); // Quita el movimiento tipo "hielo" al caminar
+        personaje.velocity = new Vector2(directionX * 7f, personaje.velocity.y);
+
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // El Rigidbody2D se saca de la jerarquia al que pertenese el objeto sobre el que esta el script, en este caso Square(player)
             // La diferencia entre los 2 es la cantidad de valores, hay que calibrarlo con la "gravity scale" y el "Salto" (Fuerza que se aplica)
 
             //GetComponent<Rigidbody2D>().AddForce(new Vector2(0, Salto));
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, Salto, 0);
+            personaje.velocity = new Vector2(personaje.velocity.x, Salto);
         }
     }
 }
